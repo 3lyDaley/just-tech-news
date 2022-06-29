fad017d9adc17765a89abe2d4aabc717460c49b9
E Daley <100460009+3lyDaley@users.noreply.github.com>
2022-06-28 20:16:07 -0700

    initial handlebar files

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
-// get all users
+
 router.get('/', (req, res) => {
   Post.findAll({
     order: [['created_at', 'DESC']],
